b929ae627c05a378e7aef167cf6a83ea8ff1ba82
joii2020 <87224197+joii2020@users.noreply.github.com>
2023-11-29 09:25:38 +0800

    ckb-auth-rs support exec (#25)
    * ckb-auth-rs support exec
    
    * ckb-auth(C Language) support exec

--- ckb-auth-rs/src/ckb_auth.rs
+++ ckb-auth-rs/src/ckb_auth.rs
@@ -5,6 +5,7 @@ use alloc::collections::BTreeMap;
 use alloc::ffi::CString;
 use alloc::format;
 use alloc::vec::Vec;
+use ckb_std::high_level::exec_cell;
 #[cfg(feature = "ckb2023")]
 use ckb_std::high_level::spawn_cell;
 use ckb_std::{
@@ -21,7 +22,7 @@ pub fn ckb_auth(
     message: &[u8; 32],
 ) -> Result<(), CkbAuthError> {
     match entry.entry_category {
-        // EntryCategoryType::Exec => ckb_auth_exec(entry, id, signature, message),
+        EntryCategoryType::Exec => ckb_auth_exec(entry, id, signature, message),
         EntryCategoryType::DynamicLinking => ckb_auth_dl(entry, id, signature, message),
         #[cfg(feature = "ckb2023")]
         EntryCategoryType::Spawn => ckb_auth_spawn(entry, id, signature, message),
@@ -48,6 +49,28 @@ fn ckb_auth_spawn(
     ];
 
     spawn_cell(&entry.code_hash, entry.hash_type, &args, 8, &mut Vec::new())?;
+    Ok(())
+}
+
+fn ckb_auth_exec(
+    entry: &CkbEntryType,
+    id: &CkbAuthType,
+    signature: &[u8],
+    message: &[u8; 32],
+) -> Result<(), CkbAuthError> {
+    let algorithm_id_str = CString::new(format!("{:02X?}", id.algorithm_id.clone() as u8,))?;
+    let signature_str = CString::new(format!("{}", encode(signature)))?;
+    let message_str = CString::new(format!("{}", encode(message)))?;
+    let pubkey_hash_str = CString::new(format!("{}", encode(id.pubkey_hash)))?;
+
+    let args = [
+        algorithm_id_str.as_c_str(),
+        signature_str.as_c_str(),
+        message_str.as_c_str(),
+        pubkey_hash_str.as_c_str(),
+    ];
+
+    exec_cell(&entry.code_hash, entry.hash_type, &args)?;
     Ok(())
 }
 
