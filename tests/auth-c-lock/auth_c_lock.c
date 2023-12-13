8ec271cd4e25b406af5b841240e9fe6eaa8c80a4
joii2020 <joii@cryptape.com>
2023-12-13 16:15:11 +0800

    Add comment

--- tests/auth-c-lock/auth_c_lock.c
+++ tests/auth-c-lock/auth_c_lock.c
@@ -221,6 +221,8 @@ int main() {
       break;
   }
 
+  // ckb_auth can be invoked multiple times for different signatures.
+  // Here we use the same one to demo the usages.
   ret = ckb_auth(&entry, &auth, lock_bytes_seg.ptr, lock_bytes_seg.size, msg32);
   if (ret) {
       return ret;
