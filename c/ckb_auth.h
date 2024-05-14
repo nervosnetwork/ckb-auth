a2f85a346a4ca5902868bf43d3dcec96bb6f7468
joii2020 <87224197+joii2020@users.noreply.github.com>
2024-05-14 09:31:46 +0800

    Remove ton (#49)

--- c/ckb_auth.h
+++ c/ckb_auth.h
@@ -85,7 +85,7 @@ enum AuthAlgorithmIdType {
     AuthAlgorithmIdSolana = 13,
     AuthAlgorithmIdRipple = 14,
     AuthAlgorithmIdSecp256R1 = 15,
-    AuthAlgorithmIdToncoin = 16,
+    // AuthAlgorithmIdToncoin = 16,
     AuthAlgorithmIdSecp256R1Raw = 17,
     AuthAlgorithmIdOwnerLock = 0xFC,
 };
